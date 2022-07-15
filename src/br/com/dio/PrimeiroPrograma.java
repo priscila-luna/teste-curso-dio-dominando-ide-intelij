8e939390aa307377f75bcad422746eb6feacd338
Priscila Luna <priscilaramos.mk@gmail.com>
2022-07-15 19:51:29 -0300

    criando class livro e alimentando class livro e gato

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -4,13 +4,47 @@ import br.com.dio.model.Gato;
 
 public class PrimeiroPrograma {
     public static void main(String[] args) {
-        Gato gato = new Gato();
-
+        Gato gato = new Gato("folgado", "malhado", 5);
         System.out.println(gato);
+
+        Livro livro1 = new Livro("BLA BLA BLA", 300);
+        System.out.println(livro1);
        /* int a = 5;
         int b = 5;
         System.out.println("Hello World! " + (a+b));*/
     }
+}
 
+class Livro {
+    private String nome;
+    private Integer numPaginas;
 
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
 }
